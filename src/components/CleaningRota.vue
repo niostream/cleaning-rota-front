0b25c2b8e966eb760be2f5a6e7314b682a1d88c3
niostream <33803205+niostream@users.noreply.github.com>
2020-01-13 20:57:10 +0900

    made a cleaning record's display

--- src/components/CleaningRota.vue
+++ src/components/CleaningRota.vue
@@ -6,116 +6,152 @@
         <h1>掃除当番表一覧</h1>
       </v-layout>
       <v-layout my-3 justify-space-around="1">
-        <v-btn outlined rounded class="green accent-1" @click="prevRecord"
-          >前月</v-btn
-        >
-        <v-btn
-          outlined
-          rounded
-          class="green accent-1"
-          @click="switchDisp = !switchDisp"
-          >表示切替</v-btn
-        >
+        <v-btn outlined rounded class="green accent-1" @click="prevRecord">前月</v-btn>
+        <v-btn outlined rounded class="green accent-1" @click="switchDisp = !switchDisp">表示切替</v-btn>
         <v-btn outlined rounded class="green accent-1">Excel出力</v-btn>
-        <v-btn outlined rounded class="green accent-1" @click="nextRecord"
-          >次月</v-btn
-        >
+        <v-btn outlined rounded class="green accent-1" @click="nextRecord">次月</v-btn>
       </v-layout>
       <v-layout my-3>
         <v-data-table
           :headers="headers"
           :items="records"
           hide-default-footer
+          items-per-page="31"
           class="elevation-1"
         >
           <template v-slot:item="row">
             <tr>
               <td>{{ row.item.date }}</td>
               <td>
-                <v-btn v-if="!row.item.kitchen && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.kitchen }}</div>
+                <div v-if="row.item.kitchen.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.kitchen.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.bath && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.bath }}</div>
+                <div v-if="row.item.bath.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.bath.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.toilet && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.toilet }}</div>
+                <div v-if="row.item.toilet.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.toilet.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.basin && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.basin }}</div>
+                <div v-if="row.item.basin.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.basin.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.cleaner && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.cleaner }}</div>
+                <div v-if="row.item.cleaner.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.cleaner.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.combustible && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.combustible }}</div>
+                <div v-if="row.item.combustible.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.combustible.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.incombustible && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.incombustible }}</div>
+                <div v-if="row.item.incombustible.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.incombustible.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.resource && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.resource }}</div>
+                <div v-if="row.item.resource.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.resource.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.electricity && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.electricity }}</div>
+                <div v-if="row.item.electricity.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.electricity.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.gas && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.gas }}</div>
+                <div v-if="row.item.gas.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.gas.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
               <td>
-                <v-btn v-if="!row.item.tap && switchDisp" class="blue"
-                  >登録</v-btn
-                >
-                <div v-else>{{ row.item.tap }}</div>
+                <div v-if="row.item.tap.user.userId == null">
+                  <v-btn v-show="switchDisp" class="blue">登録</v-btn>
+                </div>
+                <div v-else>
+                  <div>{{ row.item.tap.user.firstName }}</div>
+                  <div v-show="switchDisp">
+                    <v-btn class="red">削除</v-btn>
+                  </div>
+                </div>
               </td>
             </tr>
           </template>
         </v-data-table>
       </v-layout>
       <v-layout my-3 justify-space-around="1">
-        <v-btn outlined rounded class="green accent-1" @click="prevRecord"
-          >前月</v-btn
-        >
-        <v-btn
-          outlined
-          rounded
-          class="green accent-1"
-          @click="switchDisp = !switchDisp"
-          >表示切替</v-btn
-        >
+        <v-btn outlined rounded class="green accent-1" @click="prevRecord">前月</v-btn>
+        <v-btn outlined rounded class="green accent-1" @click="switchDisp = !switchDisp">表示切替</v-btn>
         <v-btn outlined rounded class="green accent-1">Excel出力</v-btn>
-        <v-btn outlined rounded class="green accent-1" @click="nextRecord"
-          >次月</v-btn
-        >
+        <v-btn outlined rounded class="green accent-1" @click="nextRecord">次月</v-btn>
       </v-layout>
     </v-container>
     <!-- /掃除当番表画面エリア -->
@@ -131,6 +167,8 @@ export default {
       year: "",
       month: "",
       switchDisp: true,
+      itemList: [],
+      recordList: [],
       headers: [
         {
           text: "日付",
@@ -138,36 +176,7 @@ export default {
           value: "date"
         }
       ],
-      records: [
-        {
-          date: "2020/1/1(火)",
-          kitchen: "漆原",
-          bath: "",
-          toilet: "",
-          basin: "漆原",
-          cleaner: "漆原",
-          combustible: "",
-          incombustible: "",
-          resource: "",
-          electricity: "",
-          gas: "漆原",
-          tap: ""
-        },
-        {
-          date: "2020/1/2(水)",
-          kitchen: "",
-          bath: "漆原",
-          toilet: "",
-          basin: "",
-          cleaner: "",
-          combustible: "",
-          incombustible: "",
-          resource: "",
-          electricity: "漆原",
-          gas: "",
-          tap: ""
-        }
-      ]
+      records: []
     };
   },
   created() {
@@ -175,13 +184,14 @@ export default {
     this.month = new Date().getMonth() + 1;
     // console.log("年月:", this.year, "/", this.month);
     this.getItem();
+    this.getCleaningRecord();
   },
   methods: {
     /**
      * 掃除当番表アイテム取得
      */
     async getItem() {
-      const itemList = await axios
+      this.itemList = await axios
         .get("http://localhost:8081/cleaning-rota/item-list")
         .then(res => {
           console.log(res);
@@ -191,17 +201,68 @@ export default {
           console.log(err);
           return null;
         });
-      if (itemList === null) {
+      if (this.itemList === null) {
         return;
       }
-      itemList.forEach(element => {
+      this.itemList.forEach(element => {
         this.headers.push({
           text: element.itemName,
           sortable: false,
           value: element.itemValue
         });
       });
+      // console.log("アイテム:", this.itemList);
     },
+    /**
+     * 掃除当番表レコード取得
+     */
+    async getCleaningRecord() {
+      // 初期化
+      this.recordList = [];
+      this.records = [];
+      // レコード設定
+      this.recordList = await axios
+        .get("http://localhost:8081/cleaning-rota/record", {
+          params: {
+            yearMonth:
+              new String(this.year) +
+              new String(this.month >= 10 ? this.month : "0" + this.month)
+          }
+        })
+        .then(res => {
+          console.log(res);
+          return res.data;
+        })
+        .catch(err => {
+          console.log(err);
+          return null;
+        });
+      console.log("記録:", this.recordList);
+      this.recordList.forEach(elem => {
+        let dayRecords = {
+          date: elem[0].executedDate
+        };
+        elem.forEach(data => {
+          dayRecords[data.item.itemValue] = {
+            executedDate: data.executedDate,
+            item: {
+              itemId: data.item.itemId,
+              itemName: data.item.itemName,
+              itemValue: data.item.itemValue
+            },
+            user: {
+              userId: data.user.userId,
+              firstName: data.user.firstName
+            }
+          };
+        });
+        this.records.push(dayRecords);
+      });
+      console.log("記録2:", this.records);
+    },
+    /**
+     * `前月`ボタンクリック
+     */
     prevRecord() {
       if (this.month === 1) {
         this.year -= 1;
@@ -210,7 +271,11 @@ export default {
         this.month -= 1;
       }
       // console.log("前年月:", this.year, "/", this.month);
+      this.getCleaningRecord();
     },
+    /**
+     * `次月`ボタンクリック
+     */
     nextRecord() {
       if (this.month === 12) {
         this.year += 1;
@@ -219,6 +284,7 @@ export default {
         this.month += 1;
       }
       // console.log("後年月:", this.year, "/", this.month);
+      this.getCleaningRecord();
     }
   }
 };
